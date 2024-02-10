ead7abcef26d18033ef986d8ad5baf452e8b7791
Xadarr <corentin.herisson@gmail.com>
2024-02-10 13:18:06 +0100

    HealthCheck added

--- backend/src/app.controller.ts
+++ backend/src/app.controller.ts
@@ -9,4 +9,8 @@ export class AppController {
   getHello(): string {
     return this.appService.getHello();
   }
+  @Get('/api/health')
+  checkHealth(): string {
+    return `{"status": "OK"}`;
+  }
 }
--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,12 @@
 import { NestFactory } from '@nestjs/core';
+import { NestApplication } from '@nestjs/core';
 import { AppModule } from './app.module';
+// import { CorsModule } from '@nestjs/platform-express';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3080);
+  const app = await NestFactory.create<NestApplication>(AppModule);
+
+  app.enableCors(); // This will enable CORS for all origins
+  await app.listen(5000);
 }
 bootstrap();
